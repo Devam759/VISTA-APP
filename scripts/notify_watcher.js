3c182f4026e813014b5d6c24b8dcb9e1fdc4c49d
Devam Gupta <devamg759@gmail.com>
2026-03-07 04:39:36 +0530

    improvements

--- scripts/notify_watcher.js
+++ scripts/notify_watcher.js
@@ -1,11 +1,11 @@
-const { initializeApp, credential } = require('firebase-admin');
+const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const { getMessaging } = require('firebase-admin/messaging');
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 initializeApp({
-    credential: credential.cert(serviceAccount)
+    credential: cert(serviceAccount)
 });
 
 const db = getFirestore('default');
